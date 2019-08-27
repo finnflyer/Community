deedec287d43939bd2b392038bb8dd9847e09f2d
finnflyer <finnflyer.huang@gmail.com>
2019-08-27 16:26:32 +0800

    Initial Update Day1

---
+++ src/test/java/com/finnflyer/communitystart/CommunitystartApplicationTests.java
@@ -0,0 +1,14 @@
+package com.finnflyer.communitystart;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitystartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
