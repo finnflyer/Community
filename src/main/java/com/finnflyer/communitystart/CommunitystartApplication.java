cc4f80ce93cc1d022af0407d83da130ae5c178e4
finnflyer <finnflyer.huang@gmail.com>
2019-08-27 16:26:32 +0800

    Initial Update Day1

---
+++ src/main/java/com/finnflyer/communitystart/CommunitystartApplication.java
@@ -0,0 +1,13 @@
+package com.finnflyer.communitystart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitystartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitystartApplication.class, args);
+    }
+
+}
