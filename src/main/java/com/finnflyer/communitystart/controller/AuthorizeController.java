18ccc5f10e0ab551bb904468970c609e59efbc5d
chillh <47965866@qq.com>
2019-08-29 00:58:15 +0800

    Implement H2 Database for User

--- src/main/java/com/finnflyer/communitystart/controller/AuthorizeController.java
+++ src/main/java/com/finnflyer/communitystart/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package com.finnflyer.communitystart.controller;
 
+import com.finnflyer.communitystart.Model.User;
 import com.finnflyer.communitystart.dto.AccessTokenDTO;
 import com.finnflyer.communitystart.dto.GiteeUser;
+import com.finnflyer.communitystart.mapper.UserMapper;
 import com.finnflyer.communitystart.provider.GiteeProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,6 +29,9 @@ public class AuthorizeController {
     @Value("${Gitee.redirect.uri}")
     private String redirect_uri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                                         HttpServletRequest request
@@ -37,10 +43,17 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
         String accessToken = giteeProvider.getAccessToken(accessTokenDTO);
-        GiteeUser user = giteeProvider.getUser(accessToken);
-        if (user != null) {
+        GiteeUser giteeUser = giteeProvider.getUser(accessToken);
+        if (giteeUser != null) {
             //login successfully  write cookie & session
-            request.getSession().setAttribute("user",user);
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(giteeUser.getName());
+            user.setAccount_id(String.valueOf(giteeUser.getId()));
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            userMapper.insert(user);
+            request.getSession().setAttribute("giteeUser",giteeUser);
             return "redirect:/";
 
         }else {
